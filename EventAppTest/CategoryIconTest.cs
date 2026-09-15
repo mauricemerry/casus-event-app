8f6971e065aeb0b6baa4cb2f39b9ba18bd77c97d
gronkDoBonk <2406536cuevasfragoso@zuyd.nl>
2026-09-15 12:22:19 +0200

    icon category test

---
+++ EventAppTest/CategoryIconTest.cs
@@ -0,0 +1,22 @@
+﻿using EventApp.Web.Models;
+
+namespace EventAppTest
+{
+    public class Tests
+    {
+
+        [Test]
+        public void CategoryEvent_EventCard_ReturnsCorrectCategory()
+        {
+            // Arrange
+            var eventCard = new EventCard();
+            var category = "House";
+
+            // Act
+            var result = eventCard.GetCategoryIcon(category);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("♪"));
+        }
+    }
+}
