3df8c97193d6fc3459444e5b0940ce89fcdb2fcb
Maurice Merry <mauricemerry.1998@gmail.com>
2026-09-15 11:00:09 +0100

    add test projects

---
+++ EventApp.App.Build_0.1.1.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace EventApp.App.Build_0._1._1.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
