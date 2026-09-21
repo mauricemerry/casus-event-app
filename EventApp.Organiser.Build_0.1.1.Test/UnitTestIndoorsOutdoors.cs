77bc56de8f707eaa6062ad4c3a1197fedccc3f07
sechfe <jesse.ponsens@gmail.com>
2026-09-21 10:59:17 +0200

    Create UnitTestIndoorsOutdoors.cs

---
+++ EventApp.Organiser.Build_0.1.1.Test/UnitTestIndoorsOutdoors.cs
@@ -0,0 +1,47 @@
+﻿using EventApp.OrganiserPortal.Models;
+using EventApp.OrganiserPortal.Repositories;
+using Moq;
+
+namespace EventApp.Organiser.Build_0._1._1.Test
+{
+    [TestFixture]
+    public class InOutDoorsTests
+    {
+        [Test]
+        public void GetMessage_WithValue1_ReturnsBinnen()
+        {
+            // Arrange
+            int value = 1;
+
+            // Act
+            var result = IndoorsOutdoors.GetMessage(value);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("Binnen"));
+        }
+        [Test]
+        public void GetMessage_WithValue1_Returnsbijde()
+        {
+            // Arrange
+            int value = 3;
+
+            // Act
+            var result = IndoorsOutdoors.GetMessage(value);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("Beide"));
+        }
+        [Test]
+        public void GetMessage_WithValue1_ReturnsBuiten()
+        {
+            // Arrange
+            int value = 2;
+
+            // Act
+            var result = IndoorsOutdoors.GetMessage(value);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("Buiten"));
+        }
+    }
+}
